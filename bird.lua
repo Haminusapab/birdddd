f4d5660295c917873da97fadfed3253c5ab5b91c
Haminusapab <136159416+Haminusapab@users.noreply.github.com>
2023-06-18 19:57:26 +0600

    Update bird.lua
    changes in bird behavior

--- bird.lua
+++ bird.lua
@@ -11,8 +11,8 @@ function getDistance(x1, y1, x2, y2)
    return math.sqrt((x2 - x1)^2 + (y2 - y1)^2)
 end
 
-local dx = player_x - x
-local dy = player_y - y
+local dx = math.abs(player_x - x)
+local dy = math.abs(player_y - y)
 local distance = getDistance(x, y, player_x, player_y)
 
 if distance > distanceThreshold then
